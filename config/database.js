8fce1897c3846d68d2bfbf676dba555869b8a848
rbpollock <robertbrucepollockjr@gmail.com>
2024-01-12 22:17:56 +0000

    switched to platformsh strapi template

--- config/database.js
+++ config/database.js
@@ -1,92 +1,70 @@
-const path = require('path');
+const path = require("path");
+const config = require("platformsh-config").config();
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+let dbRelationship = "postgresdatabase";
 
-  const connections = {
-    mysql: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    mysql2: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+// Strapi default sqlite settings.
+let connection = {
+  connection: {
+    client: "sqlite",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      filename: path.join(
+        __dirname,
+        "..",
+        process.env.DATABASE_FILENAME || ".tmp/data.db"
+      ),
+    },
+    useNullAsDefault: true,
+  },
+};
+
+if (config.isValidPlatform() && !config.inBuild()) {
+  // Platform.sh database configuration.
+  const credentials = config.credentials(dbRelationship);
+  console.log(
+    `Using Platform.sh configuration with relationship ${dbRelationship}.`
+  );
+
+  let pool = {
+    min: 0,
+    max: 10,
+    acquireTimeoutMillis: 600000,
+    createTimeoutMillis: 30000,
+    idleTimeoutMillis: 20000,
+    reapIntervalMillis: 20000,
+    createRetryIntervalMillis: 200,
+  };
+
+  connection = {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: credentials.ip,
+        port: credentials.port,
+        database: credentials.path,
+        user: credentials.username,
+        password: credentials.password,
+        ssl: false,
+      },
+      debug: false,
+      pool,
     },
   };
-};
+} else {
+  if (config.isValidPlatform()) {
+    // Build hook configuration message.
+    console.log(
+      "Using default configuration during Platform.sh build hook until relationships are available."
+    );
+  } else {
+    // Strapi default local configuration.
+    console.log(
+      "Not in a Platform.sh Environment. Using default local sqlite configuration."
+    );
+  }
+}
+
+console.log(connection);
+
+// export strapi database connection
+module.exports = ({ env }) => connection;
