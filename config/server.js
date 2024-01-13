21875e7f4e4b53c57b2c38e7957e70c33f11e22a
rbpollock <robertbrucepollockjr@gmail.com>
2024-01-13 00:23:14 +0000

    trying host name

--- config/server.js
+++ config/server.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   // Basic server settings.
   //
   // See https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 80),
+  host: env("HOST", "https://www.dev-54ta5gq-f5nve7b2y4734.us.platformsh.site/"),
+  port: env.int("PORT", 1337),
 
   // GraphQL endpoint configuration.
   //
