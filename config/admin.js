46c9dc77bad81dc627cb581fb681907ccd95bb71
rbpollock <robertbrucepollockjr@gmail.com>
2024-01-13 01:21:00 +0000

    adding deps

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'FoyWy6bBVqj3qvafVUxzeQ=='),
+  },
   auth: {
     secret: env("ADMIN_JWT_SECRET", "b466455d-1488-461b-9d58-1940910d8373"),
   },
